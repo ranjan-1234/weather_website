698e57dd748919bbf7a780543817bd9220e4a045
ranjan kumar <105194948+ranjan-1234@users.noreply.github.com>
2023-09-27 16:53:54 +0530

    weather_website

---
+++ index.js
@@ -0,0 +1,46 @@
+// require('dotenv').config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+const homeFile = fs.readFileSync("home.html", "utf-8");
+const replaceVal = (tempVal, orgVal) => {
+  console.log(orgVal);
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+   temperature = temperature.replace("{%location%}", orgVal.name);
+   temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+ // let p=temperature.join("")
+ //c console.log(temperature);
+  return temperature;
+};
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=Samastipur&appid=f7e7499b62047e6c1911b9aa65316585"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+    
+        const arrData = [objdata];
+       // console.log(arrData);
+        //console.log(arrData.length);
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val)).join("");
+         // console.log(realTimeData);
+        res.end(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+   else {
+    res.end("File not found");
+  }
+});
+
+server.listen(8000, "127.0.0.1");
